f2e8f7ab3d474b66454176b65a06d2c3d99c83e5
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-09-12 22:43:09 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -4,6 +4,46 @@
 #include<vector>
 using namespace std;
 
+int mySqrt(int n)
+{
+    int s = 0, e = n;
+    int ans = 0;
+    while (s <= e)
+    {
+        int mid = (s + e) >> 1;
+        if (mid * mid <= n)
+        {
+            ans = mid;
+            s = mid + 1; // go right
+        }
+        else
+            e = mid - 1; //  left jata hu
+    }
+    return ans;
+}
+
+double myPrecisionSqrt(int n)
+{
+    double sqrt = mySqrt(n); // -> O(logn)
+
+    /* O(precision) */
+    int precision = 9;
+    double step = 0.1;
+    while (precision--)
+    {
+        double j = sqrt; // j->7.0;
+        while (j * j <= n)
+        {
+            // store and compute
+            sqrt = j;
+            j += step; // j-> 7.1
+        }
+        // after this while loop i got 1 precision ans.
+        step /= 10;
+    }
+    return sqrt;
+}
+
 
 double BSPrecision(int n)
 {
