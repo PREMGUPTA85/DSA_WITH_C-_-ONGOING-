e86a92c45c3322c1dc8ffa8082d17a6f1b3c5fd7
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-15 19:32:03 +0530

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -6954,4 +6954,105 @@ int main() {
     return 0;
 }
 
-// 
+// #include<iostream>
+using namespace std;
+
+
+int main() {
+    string s1 = "prem";
+    string s2 = "love";
+
+    // cout << s1.compare(s2) << endl;
+    // if this is 0 then both are equal
+    // if this is negative then s1 < s2
+    // if this is positive then s1 > s2
+
+
+    string name = "Hello Jee Kaise ho Saare" ;
+    // string word = "Kaise ho";
+
+    // if(name.find(word) != string::npos) {
+    //     ///found
+    // }
+    // else {
+    //     //not found
+    // }
+
+    // int ans = name.find(word);
+    // cout << ans << endl;
+
+    // cout << name.substr(5);
+
+    //position and upto len
+    // cout << name.substr(5,5);
+
+    // string fName = "Love";
+    // string lName = "babbar";
+
+    // string ans = fName + " " + lName;
+    // cout << ans << endl;
+
+    // string name = "Maharana Pratap";
+    // name.clear();
+    // if(name.empty() ){
+    //     cout << "String is empty" ; 
+    // }
+    // else {
+    //     cout << "string is not empty";
+    // }
+
+
+
+    // auto it = name.begin();
+
+    // while(it != name.end()) {
+    //     cout << *it << " ";
+    //     it++;
+    // }
+    // cout << endl;
+
+
+
+    // cout << name[0] << endl;
+    // cout << name.at(0) << endl;
+
+    // cout << name.front() << endl;
+    // cout << name.back() << endl;
+    // cout << name.length() << endl;
+
+
+
+
+
+
+
+    // string sentence;
+
+    // //cin >> sentence;
+    // getline(cin, sentence, '\n');
+
+    // cout << sentence << endl;
+
+
+
+
+    // //creation
+    // string str;
+    // str.push_back('l');
+    // str.push_back('o');
+    // str.push_back('v');
+    // str.push_back('e');
+    // str.pop_back();
+    // cout << str << endl;
+    
+    // cout << "enter the input" << endl;
+    // //input
+    // cin >> str;
+    // //output
+    // cout << "Str: " << str << endl;
+    // cout << str[0] << endl;
+
+
+
+    return 0;
+}
