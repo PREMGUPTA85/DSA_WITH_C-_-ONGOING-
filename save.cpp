019d3ffa8866547b3bfddbc38066e3859f084896
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-16 09:51:19 +0530

    Implement palindrome substring counting functions
    Added functions to count palindromic substrings in a string.

--- save.cpp
+++ save.cpp
@@ -7053,6 +7053,40 @@ int main() {
     // cout << str[0] << endl;
 
 
+//-------------------------------------------------------------------------------------------------------------------------------------
+// char, arrays and strings - 2
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//this function returns the count of palindromic substrings
+//using i and j as center and exapanding around it in every iteration, if possible.
+int expandAroundCenter(string s, int i, int j) {
+        int count = 0;
+        while(i >= 0 && j <s.length() && s[i] == s[j]) {
+            count++;
+            i--;
+            j++;
+        }
+        return count;
+    }
+    int countSubstrings(string s) {
+        int totalCount = 0;
+        for(int center=0; center<s.length(); center++) {
+            //odd
+            int i = center;
+            int j = center;
+            int oddPalSubStringKaCount = expandAroundCenter(s,i,j);
+            //even
+            i = center;
+            j = center+1;
+            int evenPalSubStringKaCount = expandAroundCenter(s,i,j);
+            totalCount = totalCount + oddPalSubStringKaCount + evenPalSubStringKaCount;
+        }
+        return totalCount;
+    }
+
 
     return 0;
 }
+
