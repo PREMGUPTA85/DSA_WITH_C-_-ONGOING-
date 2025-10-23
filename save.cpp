d629db601f63e3f361ea3671cb5048bec26cde2c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-23 20:22:25 +0530

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -7664,3 +7664,10 @@ int main() {
     //  cout << "Bits in a pointer: " << sizeof(void*) * 8 << endl;
      //  float f = 5.5;
     //  float *p2 = &f;
+ //     cout << "Address of f: " << p2 << endl;
+    //     cout << sizeof(p2) << endl; // size of pointer variable
+        int *ptr2;
+        cout << *ptr2 << endl; // garbage value
+        // cout << *ptr2 << endl; // segmentation fault as ptr2 is not initialized
+    return 0;
+}
