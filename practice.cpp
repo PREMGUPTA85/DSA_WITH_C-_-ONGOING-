677f10117446826280e52eaa6c06aa21ab882d9b
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-24 22:15:06 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,1 +1,24 @@
 
+
+double myPrecisionSqrt(int n)
+{
+    double sqrt = mySqrt(n); // -> O(logn)
+
+    /* O(precision) */
+    int precision = 9;
+    double step = 0.1;
+    while (precision--)
+    {
+        double j = sqrt; // j->7.0;
+        while (j * j <= n)
+        {
+            // store and compute
+            sqrt = j;
+            j += step; // j-> 7.1
+        }
+        // after this while loop i got 1 precision ans.
+        step /= 10;
+    }
+    return sqrt;
+}
+
