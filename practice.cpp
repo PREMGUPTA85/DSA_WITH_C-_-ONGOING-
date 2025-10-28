735978088b865e3e5c444a9d8053af7de1b2120f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-28 19:55:55 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,57 +1,20 @@
-// friend class
-// frined keyword in cpp 
-#include<iostream>
-using namespace std;
-
-// freind class is a class that can access private and protected members of another class in which it is declared as a friend.
-
-class A{
-    private: 
-    int x;
-
-public:
-    A(int _x) : x(_x) {};
-
-int getX() const {
-        return x;
-    }   
-
-void setX(int val){
-        x = val;
-    }   
-
-
-void print() const{
-        cout << x << endl;
+int binarysearch(vector<int> &nums, int target){
+    int n = nums.size();
+    int start = 0;
+    int end = n - 1;
+    while(start <= end){
+        int mid = start + (end - start)/2;
+         if(nums[mid] == target){
+            return mid;
+        }
+        else if(target > nums[mid]){
+            // right side me jao
+            start = mid + 1;
+        }
+        else if(target < nums[mid]){
+            end = mid - 1;
+        }
+        // mid = (start + end)/2;
     }
-
-// for accessing private members of class A in class B
-    friend class B;
-
-    friend void print(const A &a);
-};
-
-class B{
-    public:
-    void print(const A &a){
-        // cout << a.getX() << endl;
-        // cout << a.x << endl; // accessing private member of class A
-
-        // better hoga 
-        a.print();
-    }
-};
-
-// void print(const A &a){
-//     cout << a.x << endl; // accessing private member of class A
-// }
-
-
-int main() {
-    A c(5);
-    B b;
-    c.print();
-    b.print(c);
-    //print(c);
-    return 0;
+    return -1;
 }
