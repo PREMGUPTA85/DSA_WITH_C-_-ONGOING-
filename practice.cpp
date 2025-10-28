bf65b6c99d757d099a001703dca87451239dd1ef
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-28 20:02:25 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -80,3 +80,28 @@ int main() {
 
 #include<iostream>
 using namespace std;
+
+
+void findlastOccurence(int nums[],int n, int target, int &ansindex){l
+    int start = 0;
+    int end = n - 1;
+    while(start <= end){
+        int mid = start + (end - start)/2;
+         if(nums[mid] == target){
+            // ans found --> may or may not be first occurence
+            // store and compute
+            ansindex = mid;
+            // kyoki first occurence ki baat ho rhi h toh left me hi jana pdega
+            start = mid + 1;
+        }
+        else if(target > nums[mid]){
+            // right side me jao
+            start = mid + 1;
+        }
+        else if(target < nums[mid]){
+            end = mid - 1;
+        }
+        // mid = (start + end)/2;
+    }
+}
+
