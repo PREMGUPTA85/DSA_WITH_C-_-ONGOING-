8c69e24db368c3d6ed5e284fb47e249ef3cb9aa4
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-29 20:29:01 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -2,3 +2,19 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+
+class Solution {
+public:
+    bool isPossibleSolution(vector<int> &stalls, int k, int mid) {
+        int c = 1;           // count of cows placed
+        int pos = stalls[0]; // position of last placed cow
+
+        for (int i = 1; i < stalls.size(); i++) {
+            if (stalls[i] - pos >= mid) {
+                c++;
+                pos = stalls[i];
+            }
+            if (c == k) return true;
+        }
+        return false;
+    }
