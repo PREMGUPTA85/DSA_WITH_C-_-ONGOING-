d7ae310423d6cc03a53cd98245dd9979bcf25002
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-10-31 22:30:23 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,31 +1,5 @@
 
-// 53.
-/*
-5
-********1********
-*******2*2*******
-******3*3*3******
-*****4*4*4*4*****
-****5*5*5*5*5****
-*/
+// In container's vector part cover up
+// C++ with STL :-- (contgainer's) -- 1.vector part 
 #include<iostream>
 using namespace std;
-
-
-int main () {
-    int n;
-    cin >> n;
- for(int i = 0; i < n; i++){
-        for(int j = 0; j < 2 * n - i - 2; j++){
-            cout << "*";
-        }
-for(int j = 0; j <=i ; j++){
-            cout << i + 1;
-            cout << "*" ;
-        }
-for(int j = 0; j < 2 * n - i - 3; j++){
-            cout << "*";
-        }
-cout << endl;
-    }
-}
