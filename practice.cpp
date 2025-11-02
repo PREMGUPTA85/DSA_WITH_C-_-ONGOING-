783a63a06a55c42f03d7cc59971d3c5cad539e31
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-02 10:29:48 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -50,3 +50,20 @@ Node* createList(int n) {
     }
     return head;
 }
+
+
+
+int sumOfAlternate(Node* head) {
+    int sum = 0;
+    bool take = true;
+    while (head != nullptr) {
+        if (take) sum += head->data;
+        take = !take;
+        head = head->next;
+    }
+    return sum;
+}
+
+int main() {
+    int n;
+   
