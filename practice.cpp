eefb5702abb81e27a1404d6d4e48fd46ba0784a1
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-09 16:13:02 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -7,4 +7,7 @@ public:
 
         for(int i=0; i<nums.size(); i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-           
+            int l=i+1, r=nums.size()-1;
+
+            while(l<r){
+              
