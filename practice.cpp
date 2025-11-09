e9dc4997517a4fbf74180f8d93cc13ce250327a4
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-10 00:33:50 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,31 +1,12 @@
-class Solution {
+
+class Solution
+{
 public:
-   
- vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
+    Node *solve(Node *head)
+    {
+        auto it = head;
+        auto tail = it; // for tracking tail node.
 
-        for(int i=0; i<nums.size(); i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int l=i+1, r=nums.size()-1;
-
-            while(l<r){
-                int sum = nums[i] + nums[l] + nums[r];
-                if(sum<0) l++;
-                else if(sum>0) r--;
-                else{
-                    ans.push_back({nums[i], nums[l], nums[r]});
-                    l++; r--;
-                    while(l<r && nums[l]==nums[l-1]) l++;
-                    while(l<r && nums[r]==nums[r+1]) r--;
-                }
-            }
-        }
-        return ans;
-    }
-};
-
-
-
-
-
+        while (it)
+        {
+           
