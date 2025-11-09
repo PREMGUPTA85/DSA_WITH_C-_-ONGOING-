c90110a869ee3eb806df015157a9298a457f2048
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-09 16:11:53 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,24 +1,3 @@
-
-
-// wap to convert decimal into binary using backtracking 
-#include<iostream>
-using namespace std;
-
-
-
-int binaryconversion(int num){
-    if(num > 0){
-        binaryconversion(num / 2);
-        cout << num % 2;
-    }
-}
-
-
-int main(){
-    int num;
-    cin >> num;
-    binaryconversion(num);
-    return 0;
-}
-
-
+class Solution {
+public:
+   
