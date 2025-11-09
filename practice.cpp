f997daf517f7fde9ee0bd79f2d0307dc4db96dd7
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-09 16:12:36 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -3,4 +3,7 @@ public:
    
  vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-       
+        vector<vector<int>> ans;
+
+        for(int i=0; i<nums.size(); i++){
+   
