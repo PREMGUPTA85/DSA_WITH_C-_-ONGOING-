37d3a51412eacc9ead4b466e98118fe71bce8a14
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-09 16:14:25 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -18,4 +18,5 @@ public:
                     l++; r--;
                     while(l<r && nums[l]==nums[l-1]) l++;
                     while(l<r && nums[r]==nums[r+1]) r--;
-             
+                }
+           
