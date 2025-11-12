3e999916db0a7adc86939c39e35b04326a3e4a21
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-12 23:57:21 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -4,3 +4,6 @@ void insertAtPosition(int position,int value, Node* &head, Node* &tail ){
   int length = getLength(head);
 if(position == 1) {
     
+//insert at ahead krna chahta hu 
+    head = insertAtHead(value, head, tail);
+ 
