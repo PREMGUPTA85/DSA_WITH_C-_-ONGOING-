65543cca3d5d9f4036bd46b26881fe002c0049a8
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-14 14:07:22 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,18 +1,5 @@
-//134.Divide two integers without using / or % and return the quotient (truncate toward zero).
-int Quotient(int dividend, int divisor){
-    int index = -1;
-    int start = 0;
-    int end = dividend;
-    int quotient = start + ((end - start) >> 1);
-    while(start <= end){
-        if(divisor * quotient == dividend){
-            return quotient;
-        }
-        else if(divisor * quotient < dividend){index = quotient;
-            start = quotient + 1;
-        }
-        else  end = quotient - 1;
-        quotient = start + (end - start) / 2;
-    }
-    return index;
-}
+
+//132.Search target in a nearly sorted array where each element may be misplaced by ±1 index
+int nearlysearch(int arr[],int size,int target){
+    int s=0,e=size-1,mid=s+(e-s)/2;
+   
