23f3643f690454288b5b6904eaa2f21380493a9c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-14 14:07:38 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -2,4 +2,5 @@
 //132.Search target in a nearly sorted array where each element may be misplaced by ±1 index
 int nearlysearch(int arr[],int size,int target){
     int s=0,e=size-1,mid=s+(e-s)/2;
-   
+    while(s<=e){
+     
