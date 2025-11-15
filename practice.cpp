85420e2c4e2cf04877666219e59c6d60a19e376b
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-15 16:49:24 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,19 +1,3 @@
-
-//132.Search target in a nearly sorted array where each element may be misplaced by ±1 index
-int nearlysearch(int arr[],int size,int target){
-    int s=0,e=size-1,mid=s+(e-s)/2;
-    while(s<=e){
-        if(arr[mid]==target) return mid;
-        if(arr[mid-1]==target) return mid-1;
-        if(arr[mid+1]==target) return mid+1;
-        if(target>arr[mid]) s=mid+2;
-        else e=mid-2;
-        mid=s+(e-s)/2;
-}    return -1;
-}
-
-
-
-    
-
-
+class Solution {
+public:
+   
