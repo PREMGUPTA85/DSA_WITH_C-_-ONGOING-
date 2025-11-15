27ac03af56300d306e0420c9cfa21e687dac4f49
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-15 16:50:43 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -5,3 +5,7 @@ public:
     // --- Step 1: First window sum calculate ---
         int i = 0, j = k - 1;
         int sum = 0;
+
+        for(int x = i; x <= j; x++){
+            sum += nums[x];
+     
