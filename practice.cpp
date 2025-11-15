71967415d61ffcb555943276587dfd17a7e3ef1f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-15 16:49:55 +0530

    Add comment for first window sum calculation
    Added a comment to indicate the start of the first window sum calculation.

--- practice.cpp
+++ practice.cpp
@@ -2,5 +2,6 @@ class Solution {
 public:
    
  double findMaxAverage(vector<int>& nums, int k) {
-
-    
+    // --- Step 1: First window sum calculate ---
+        int i = 0, j = k - 1;
+        int sum = 0;
