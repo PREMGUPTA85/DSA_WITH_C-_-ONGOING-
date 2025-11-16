5b2d30a4c24f7296fb91a6e69bee24388b18f76f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-16 19:44:05 +0530

    Fix pointer manipulation in linked list reversal

--- practice.cpp
+++ practice.cpp
@@ -7,7 +7,8 @@ Node* reverse(Node* head) {
  while (curr != NULL) {
         next = curr->next; // Store next node
     curr->next = prev; // Reverse current node's pointer
-   
+        prev = curr;       // Move pointers one position ahead
+      
     
 
    
