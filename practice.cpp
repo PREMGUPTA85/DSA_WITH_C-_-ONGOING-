477ea9ac4397e502605a7c2dd74b4f47528b5bbe
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-16 19:44:27 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -8,7 +8,8 @@ Node* reverse(Node* head) {
         next = curr->next; // Store next node
     curr->next = prev; // Reverse current node's pointer
         prev = curr;       // Move pointers one position ahead
-      
+        curr = next;
+  
     
 
    
