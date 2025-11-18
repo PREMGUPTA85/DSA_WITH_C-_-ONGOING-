5875ae16155ceb12335ed33b88a45013f3b31753
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-18 17:41:40 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,37 +1,9 @@
-// Merge two sorted linked lists
-class Solution {
+class Solution
+{
 public:
-    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
+    Node *solve(Node *head)
+    {
+        auto it = head;
+        auto tail = it; // for tracking tail node.
 
-        if (!head1) return head2;
-        if (!head2) return head1;
-
-        ListNode* mergedHead = nullptr;
-
-        if (head1->val < head2->val) {
-            mergedHead = head1;
-            head1 = head1->next;
-        } else {
-            mergedHead = head2;
-            head2 = head2->next;
-        }
-
-        ListNode* mergedTail = mergedHead;
-
-        while (head1 && head2) {
-            if (head1->val < head2->val) {
-                mergedTail->next = head1;
-                head1 = head1->next;
-            } else {
-                mergedTail->next = head2;
-                head2 = head2->next;
-            }
-            mergedTail = mergedTail->next;
-        }
-
-        if (head1) mergedTail->next = head1;
-        else mergedTail->next = head2;
-
-        return mergedHead;
-    }
-};
+       
