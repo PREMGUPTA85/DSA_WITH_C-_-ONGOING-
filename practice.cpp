6dcf870ef0049a31217173f7b739bd001b47078e
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-19 23:39:44 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,14 @@
 
+int sumOfAlternate(Node* head) {
+    int sum = 0;
+    bool take = true;
+    while (head != nullptr) {
+        if (take) sum += head->data;
+        take = !take;
+        head = head->next;
+    }
+    return sum;
+}
 
 int main() {
     int n;
