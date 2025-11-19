bd1d7dc14c5ca61b6cb4a43fc43ae0be20f74f6c
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-19 23:47:02 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,10 @@
 
+
+Node* createList(int n) {
+    Node* head = nullptr, *temp = nullptr;
+    for (int i = 0; i < n; i++) {
+        Node* newNode = new Node();
+        cin >> newNode->data;
         newNode->next = nullptr;
 
         if (head == nullptr)
