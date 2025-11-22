69f7b2f5b49dbe14466265c7dde5e96b07733ea1
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-22 19:05:48 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -41,6 +41,12 @@ int main() {
     cout << "Enter number of nodes: ";
     cin >> n;
     cout << "Enter elements:\n";
+    Node* head = createList(n);
+
+    cout << "Sum of alternate nodes (starting from first): " << sumOfAlternate(head);
+    return 0;
+}
+
  
 
 
