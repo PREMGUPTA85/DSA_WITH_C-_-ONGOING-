1a6872fbe08444a3655e4ca5fb014df54fad53db
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-23 20:08:36 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,52 +1,6 @@
-// waap to find sum of altrnates valuesin starting from first node 
-#include<iostream>
-using namespace std;
 
-struct Node {
-    int data;
-    Node* next;
-};
-
-Node* createList(int n) {
-    Node* head = nullptr, *temp = nullptr;
-    for (int i = 0; i < n; i++) {
-        Node* newNode = new Node();
-        cin >> newNode->data;
-        newNode->next = nullptr;
-
-        if (head == nullptr)
-            head = temp = newNode;
-        else {
-            temp->next = newNode;
-            temp = newNode;
-        }
-    }
-    return head;
-}
-
-int sumOfAlternate(Node* head) {
-    int sum = 0;
-   bool take = true;
-    while (head != nullptr) {
-        if (take) sum += head->data;
-        take = !take;
-        head = head->next;
-    }
-    return sum;
-}
-
-
-int main() {
-    int n;
-    cout << "Enter number of nodes: ";
-    cin >> n;
-    cout << "Enter elements:\n";
-    Node* head = createList(n);
-
-    cout << "Sum of alternate nodes (starting from first): " << sumOfAlternate(head);
+    int **r = q;
+    int ***s = &q;
+    
     return 0;
 }
-
- 
-
-
