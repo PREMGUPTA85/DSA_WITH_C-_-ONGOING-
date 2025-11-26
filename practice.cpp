e23449ab276b2199ab5e0eccac31d6a4ef6407cf
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-26 21:48:29 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,31 +1,4 @@
-// today
-// done
-// ongoing
-// pointer class -2
-#include<iostream>
-using namespace std;
 
-// double pointer level
-int main() {
-    int a = 5;
-    int *p = &a;      // pointer to an int
-    int **q = &p;     // pointer to pointer to an int
-
-    cout << "Value of a: " << a << endl;
-    cout << "Address of a: " << &a << endl;
-
-    cout << "Value of p (address of a): " << p << endl;
-    cout << "Value pointed by p (*p): " << *p << endl;
-
-    cout << "Value of q (address of p): " << q << endl;
-    cout << "Value pointed by q (*q, which is address of a): " << *q << endl;
-    cout << "Value pointed by the pointer pointed by q (**q, which is value of a): " << **q << endl;
-
-    int a = 50;
-    int *p = &a;
-    int **q = &p;
-    int **r = q;
-    int ***s = &q;
-    
-    return 0;
+  
+  return 0;
 }
