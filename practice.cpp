2b80a5108c153b61cc36521c51a36aa15c164d03
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-29 18:51:35 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,25 @@
 
+        void print() {
+          cout << "Top1: " << top1 << endl;
+          cout << "Top2: " << top2 << endl;
+          for(int i=0; i<size; i++) {
+            cout << arr[i] << " ";
+          }
+          cout << endl;
+        }
+};
+
+int main() {
+  Stack s(5);
+  s.push1(10);
+  s.print();
+  s.push2(90);
+  s.print();
+  s.push2(80);
+  s.print();
+  s.pop1();
+  s.print();
+  s.push2(90);
   s.print();
   s.push2(80);
   s.print();
