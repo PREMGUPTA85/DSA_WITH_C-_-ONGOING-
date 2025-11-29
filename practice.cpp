37643a35063174de0d9deb3467c70840af63a718
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-29 18:51:02 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,23 +1,10 @@
-#include <iostream>
-#include<stack>
-using namespace std;
 
-
-int main() {
-  stack<char> s;
-  string name = "amitwala";
-
-  //inseration of characters into the stack
-  for(int i=0; i<name.length(); i++) {
-    char ch = name[i];
-    s.push(ch);
-  }
-
-  //retrieval of characters from the stack 
-  while(!s.empty()) {
-    cout << s.top();
-    s.pop();
-  }
-
+  s.print();
+  s.push2(80);
+  s.print();
+  s.push2(80);
+  s.print();
+  s.push1(80);
+  s.print();
   return 0;
 }
