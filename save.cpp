cade250f738cd59bc0e84cd8b91688f911b94d46
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-11-30 15:04:28 +0530

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -9306,3 +9306,306 @@ public:
         return head;
     }
 };
+
+
+// ----------------------------Stack class - 1--------------------------------
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+  //creation
+  stack<int> s;
+  //insertion
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  //size
+  cout << s.size() << endl;
+  //check empty or not 
+  cout << s.empty() << endl;
+  //peek 
+  cout << s.top() << endl;
+  //removal 
+  s.pop();
+  cout << s.top() << endl;
+  
+  return 0;
+}
+
+//#include <iostream>
+using namespace std;
+
+class Stack{
+    public:
+          int* arr;
+          int size;
+          int top;
+
+          Stack(int capacity) {
+            arr = new int[capacity];
+            size = capacity;
+            top = -1;
+          }
+
+          void push(int val) {
+              if(top == size-1) {
+                //astack is already full, agar insert karoge, toh stack overflow hojaega
+                cout << "Stack Overflow" << endl;
+              }
+              else {
+                //normal case -> stack me khaali jagah hai abhi
+                top++;
+                arr[top] = val;
+              }
+          }
+
+          void pop() {
+              if(top == -1) {
+                //stack is empty, cannot pop in this case, coz there is no elemnt to pop
+                cout << "Stack Underflow" << endl;
+              }
+              else {
+                //normal case
+                arr[top] = 0;
+                top--;
+              }
+          }
+
+          int getSize() {
+              return top+1;
+          }
+
+          bool isEmpty() {
+              if(top == -1) {
+                return true;
+              }
+              else {
+                return false;
+              }
+          }
+
+          int getTop() {
+              if(top == -1) {
+                cout << "There is no element at the top, as stack is empty";
+                return -1;
+              }
+              else {
+                //normal case
+                return arr[top];
+              }
+          }
+
+          void print() {
+            cout << "Printing Stack" << endl;
+            for(int i=0; i<size; i++) {
+              cout << arr[i] << " " ;
+            }cout << endl;
+          }
+};
+
+
+int main() {
+  //creating a Stack of initial size 5 
+  Stack s(5);
+  s.print();
+  s.push(10);
+  s.print();
+  s.push(20);
+  s.print();
+  s.push(30);
+  s.print();
+  s.push(40);
+  s.print();
+  s.push(50);
+  s.print();
+  s.push(60);
+  s.print();
+
+  cout << s.getTop() << endl;
+  cout << s.getSize() << endl;
+  cout << s.isEmpty() << endl;
+
+  s.pop();
+  s.print();
+    s.pop();
+  s.print();
+    s.pop();
+  s.print();
+      s.pop();
+  s.print();
+      s.pop();
+  s.print();
+  s.pop();
+  cout << s.getSize();
+
+
+
+  return 0;
+}
+
+// hw stack using linked list 
+
+// implement array using double stack
+#include <iostream>
+using namespace std;
+class Stack{
+  public:
+        int* arr;
+        int size;
+        int top1; 
+        int top2;
+
+        Stack(int capacity) {
+          arr= new int[capacity];
+          size = capacity;
+          top1 = -1;
+          top2 = size;
+        }
+
+        void push1(int value) {
+          //if space available, then push
+          //else stack overflow karjaega
+          if(top2-top1 == 1) {
+            //cannot insert
+            cout << "Stack Overflow"<<endl;
+          }
+          else {
+            top1++;
+            arr[top1] = value;
+          }
+        }
+
+        void push2(int value) {
+          if(top2-top1 == 1) {
+            cout << "Stack Overflow" << endl;
+          }
+          else {
+            top2--;
+            arr[top2] = value;
+          }
+
+        }
+
+        void pop1() {
+          //if element available then pop
+          //if stack empty, then dont pop
+          if(top1 == -1){
+            //stack 1 is empty
+            cout << "Stack Underflow" << endl;
+          }
+          else {
+            arr[top1] = 0;
+            top1--;
+          }
+        }
+
+        void pop2() {
+            if(top2 == size) {
+              //stack 2 is empty, no element to pop
+              cout << "Stack Underflow" << endl;
+            }
+            else {
+              arr[top2] = 0;
+              top2++;
+            }
+        }
+
+        void print() {
+          cout << "Top1: " << top1 << endl;
+          cout << "Top2: " << top2 << endl;
+          for(int i=0; i<size; i++) {
+            cout << arr[i] << " ";
+          }
+          cout << endl;
+        }
+};
+
+int main() {
+  Stack s(5);
+  s.push1(10);
+  s.print();
+  s.push2(90);
+  s.print();
+  s.push2(80);
+  s.print();
+  s.pop1();
+  s.print();
+  s.push2(90);
+  s.print();
+  s.push2(80);
+  s.print();
+  s.push2(80);
+  s.print();
+  s.push1(80);
+  s.print();
+  return 0;
+}
+
+#include <iostream>
+#include<stack>
+using namespace std;
+
+int main() {
+  stack<char> s;
+  string name = "amitwala";
+
+  //inseration of characters into the stack
+  for(int i=0; i<name.length(); i++) {
+    char ch = name[i];
+    s.push(ch);
+  }
+
+  //retrieval of characters from the stack 
+  while(!s.empty()) {
+    cout << s.top();
+    s.pop();
+  }
+
+  return 0;
+}
+
+// 
+#include <iostream>
+#include<stack>
+using namespace std;
+
+void insertAtBottom(stack<int>& s, int value) {
+  //base case
+  if(s.empty()) {
+    s.push(value);
+    return;
+  }
+  //1 case hum krenge and baaki recursion karega
+  int topElement = s.top();
+  s.pop();
+
+  //baaki recursion
+  insertAtBottom(s,value);
+
+  //backtrack
+  s.push(topElement);
+}
+
+int main() {
+  stack<int> s;
+  s.push(10);
+  s.push(20);
+  s.push(30);
+  s.push(40);
+  s.push(50);
+
+  int value = 13;
+
+  insertAtBottom(s,value);
+
+  //traverse
+  while(!s.empty()) {
+    cout << s.top() << " ";
+    s.pop();
+  }
+  cout << endl;
+
+
+  return 0;
+}
