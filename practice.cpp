6d6062b87ada3cb3956bf0304dfd4ffe58a2e72b
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-02 22:45:49 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,36 +1,11 @@
-#include <iostream>
-#include<stack>
-using namespace std;
 
-void printMiddle(stack<int>& s, int count ) {
-  //base case {
-    if(count == 0) {
-      cout << s.top() << endl;
-      return;
+
+  void pop(){
+    if(top == -1) cout << "Stack Underflow" << endl;
+    else {
+      arr[top] = 0;
+      top--;
     }
+  }
 
-    //1 case solve krna h 
-    int topElement = s.top();
-    s.pop();
-    count--;
-
-    //baaki rec
-    printMiddle(s,count);
-
-    //backtrack
-    s.push(topElement);
-}
-
-
-int main() {
-  stack<int> s;
-  s.push(10);
-  s.push(20);
-  s.push(30);
-  s.push(40);
-  s.push(50);
-  s.push(60);
-  int count = s.size()/2;
-  printMiddle(s,count);
-  return 0;
 }
