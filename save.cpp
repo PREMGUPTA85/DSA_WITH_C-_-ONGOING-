46cb8cc96e2b1df679c5d609ac677c354977cb1a
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-07 01:40:30 +0530

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -1,4 +1,19 @@
 
+    cout << "Prem's age is: " << age << endl;
+    return 0;
+}
+
+// 3.wap to store values in all data types variables (int,bool,char,float,double)
+#include<iostream>
+using namespace std;
+int main () {
+    int age = 12;
+    float height = 8.4;
+    double weight = 75.05;
+    char alphabet = 'A';
+    bool ismale = true;
+    bool isboy = 1;
+    bool isfemale = false;
     bool isgirl = 0;
     cout << age << endl;
     cout << height << endl;
