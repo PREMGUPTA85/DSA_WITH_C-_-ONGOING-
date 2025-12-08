2844d27e9776e57adeace430266e052ce388bafa
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-08 22:04:46 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,68 +1,96 @@
 
-class Stack{
-    public:
-          int* arr;
-          int size;
-          int top;
+    // ll is not empty 
+    else {
+        newnode->next = head ;
+        head = newnode;
+    }
+}
 
-          Stack(int capacity) {
-            arr = new int[capacity];
-            size = capacity;
-            top = -1;
-          }
+// insert at tail in linked list
+void insertattail(Node* &head, Node* &tail, int val){
+   
+    Node* newnode = new Node(val);
+     // initially if ll is empty
+    if(head == NULL){
+        insertathead(head, tail, val);
+    }
 
-          void push(int val) {
-              if(top == size-1) {
-                //astack is already full, agar insert karoge, toh stack overflow hojaega
-                cout << "Stack Overflow" << endl;
-              }
-              else {
-                //normal case -> stack me khaali jagah hai abhi
-                top++;
-                arr[top] = val;
-              }
-          }
+    else{
+        tail->next = newnode;
+        tail = newnode;
+    }
+}
 
-          void pop() {
-              if(top == -1) {
-                //stack is empty, cannot pop in this case, coz there is no elemnt to pop
-                cout << "Stack Underflow" << endl;
-              }
-              else {
-                //normal case
-                arr[top] = 0;
-                top--;
-              }
-          }
+void printll(Node* &head){
+    Node* temp = head;
+    while(temp != NULL){
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
 
-          int getSize() {
-              return top+1;
-          }
+int getlength(Node* &head){
+    Node* temp = head;
+    int len = 0;
+    while(temp != NULL){
+        temp = temp->next;
+        len++;
+    }
+    return len;
+}
 
-          bool isEmpty() {
-              if(top == -1) {
-                return true;
-              }
-              else {
-                return false;
-              }
-          }
 
-          int getTop() {
-              if(top == -1) {
-                cout << "There is no element at the top, as stack is empty";
-                return -1;
-              }
-              else {
-                //normal case
-                return arr[top];
-              }
-          }
+// insert at position 
+void insertatposition(int position, int val, Node* &head, Node* &tail){
+    Node* temp = head;
+    int len = getlength(head);
+    if(head == NULL){
+        Node* newnode = new Node(val);
+        head = newnode;
+        tail = newnode;
+    }
+    else if(position == 1){
+        insertathead(head, tail, val);
+    }
+    else if(position == len + 1){
+        insertattail(head, tail, val);
+    }
+    for(int i = 0; i < position - 2; i++){
+        temp = temp->next;
+    }
+    // ab to aa gye n 10 pe 
+    Node* newnode = new Node(val);
+    newnode->next = temp->next;
+    temp->next =newnode;
+}
 
-          void print() {
-            cout << "Printing Stack" << endl;
-            for(int i=0; i<size; i++) {
-              cout << arr[i] << " " ;
-            }cout << endl;
-          }
-};
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    insertathead(head, tail, 5); // ll is 5
+    insertathead(head, tail, 10); // ll is 10-> 5
+    insertathead(head, tail, 0); // ll is 15->10->5
+    // current ll: 15->10->5
+
+    insertattail(head, tail, 5);  //  l: 15-l>10->5-> 5
+    insertattail(head, tail, 10);  //  l: 15-l>10->5-> 5 ->10
+    insertattail(head, tail, 0); //l: 15-l>10->5-> 5->10->15
+    printll(head);
+
+    cout << "Length is: " << getlength(head) << endl;
+    insertatposition(1, 11, head, tail); 
+    printll(head);
+    insertatposition(2, 12, head, tail); 
+    printll(head);
+    insertatposition(3, 13, head, tail); 
+    printll(head);
+    insertatposition(4, 14, head, tail); 
+    printll(head);
+    insertatposition(5, 15, head, tail); 
+    printll(head);
+    insertatposition(6, 16, head, tail); 
+    printll(head);
+    return 0;
+}
