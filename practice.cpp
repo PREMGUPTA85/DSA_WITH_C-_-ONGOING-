a94486b49304deb2e99336b59ff59b754da54e04
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-09 20:23:16 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,16 @@
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution{
+public:
+    ListNode* hasCycle(ListNode *head){
+        ListNode *slow=head;
         ListNode *fast=head;
         while(fast!=NULL){
             fast=fast->next;
