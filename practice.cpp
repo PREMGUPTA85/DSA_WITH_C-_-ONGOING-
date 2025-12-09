12f7e4b0a3b16c24312a659cb33eb9316850c1f3
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-09 20:21:25 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,39 +1,4 @@
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution{
-public:
-    ListNode* hasCycle(ListNode *head){
-        ListNode *slow=head;
-        ListNode *fast=head;
-        while(fast!=NULL){
-            fast=fast->next;
-            if(fast!=NULL){
-                fast=fast->next;
-                slow=slow->next;
-                if(fast==slow){
-                    return fast;
-                }
-            }
-        }
-        return nullptr;
-    }
-
-    ListNode* detectCycle(ListNode *head){
-        ListNode *fast=hasCycle(head);
-        if(!fast)return nullptr;
-        ListNode *slow=head;
-        ListNode *prev=0;
-        while(slow!=fast){
-            slow=slow->next;
-            prev=fast;
-            fast=fast->next;
         }
         prev->next=nullptr;
         return slow;
