bd56da6b807c7cd975109cc61c2a0f23e9991c3b
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-09 20:02:53 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,57 @@
 
 
+    ListNode* detectCycle(ListNode *head){
+        ListNode *fast=hasCycle(head);
+        if(!fast)return nullptr;
+        ListNode *slow=head;
+        ListNode *prev=0;
+        while(slow!=fast){
+            slow=slow->next;
+            prev=fast;
+            fast=fast->next;
+        }
+        prev->next=nullptr;
+        return slow;
+    }
+};
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution{
+public:
+    vector<ListNode*> splitListToParts(ListNode* head,int k){
+        int N=0;
+        auto it=head;
+        while(it){
+            N++;
+            it=it->next;
+        }
+        int partSize=N/k;
+        int extraNodes=N%k;
+        vector<ListNode*> ans(k,nullptr);
+        it=head;
+        for(int i=0;i<k&&it;i++){
+            ans[i]=it;
+            int currentPartSize=partSize+(extraNodes-- >0?1:0);
+            for(int j=0;j<currentPartSize-1;j++) it=it->next;
+            auto nextPartStarting=it->next;
+            it->next=nullptr;
+            it=nextPartStarting;
+        }
+        return ans;
+    }
+};
+
+
 /*
 // Definition for a Node.
 class Node {
