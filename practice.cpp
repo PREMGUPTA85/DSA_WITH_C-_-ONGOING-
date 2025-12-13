8addd9aad3159de2a3ac6fae3189317f046b941f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-13 18:43:44 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,45 +1,14 @@
 
-
-// heap sort implementation in C++
-#include <iostream>
-using namespace std;
-
-void heapify(int arr[], int n, int i) {
-    int largest = i; 
-    int left = 2 * i + 1; 
-    int right = 2 * i + 2; 
-
-    if (left < n && arr[left] > arr[largest])
-        largest = left;
-    if (right < n && arr[right] > arr[largest])
-        largest = right;
-
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
+            if(left1 <= right2 && left2 <= right1){
+                if((n + m) % 2 == 0)
+                    return (max(left1, left2) + min(right1, right2)) / 2.0;
+                else
+                    return max(left1, left2);
+            }
+            else if(left1 > right2) high = cut1 - 1;
+            else low = cut1 + 1;
+        }
+        return 0.0;
     }
-}
+};
 
-void heapSort(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--)
-        heapify(arr, n, i);
-
-    for (int i = n - 1; i >= 0; i--) {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
-}
-
-int main() {
-    int arr[] = {12, 11, 13, 5, 6, 7};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    heapSort(arr, n);
-
-    cout << "Sorted array is \n";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-    cout << endl;
-    return 0;
-}
-  
