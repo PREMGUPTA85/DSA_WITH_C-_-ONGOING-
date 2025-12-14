31f91657c3eea9d77699678bf7e32144083265eb
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-14 19:49:43 +0530

    Update save.cpp

--- save.cpp
+++ save.cpp
@@ -1,13 +1,4 @@
-//1. WAP to print Namaste duniya 
-#include<iostream>
-using namespace std;
-int main () {
-    cout << "namaste duniya" << endl;
-    return 0;
-}
 
-// 2.wap to access the variable by declaring it 
-#include<iostream>
 using namespace std;
 int main () {
     int age = 19;
