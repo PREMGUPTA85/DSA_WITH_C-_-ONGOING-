7fd287b750e4c4ee3c9592b3c78ace657669ae11
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-16 23:54:32 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,30 +1,13 @@
-// Median of Two Sorted Arrays
-class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& a, vector<int>& b) {
-        if(a.size() > b.size()) return findMedianSortedArrays(b, a);
-        int n = a.size(), m = b.size();
-        int low = 0, high = n;
 
-        while(low <= high){
-            int cut1 = (low + high) / 2;
-            int cut2 = (n + m + 1)/2 - cut1;
-
-            int left1 = (cut1 == 0) ? INT_MIN : a[cut1-1];
-            int left2 = (cut2 == 0) ? INT_MIN : b[cut2-1];
-            int right1 = (cut1 == n) ? INT_MAX : a[cut1];
-            int right2 = (cut2 == m) ? INT_MAX : b[cut2];
-
-            if(left1 <= right2 && left2 <= right1){
-                if((n + m) % 2 == 0)
-                    return (max(left1, left2) + min(right1, right2)) / 2.0;
-                else
-                    return max(left1, left2);
-            }
-            else if(left1 > right2) high = cut1 - 1;
-            else low = cut1 + 1;
-        }
-        return 0.0;
+    ~derived(){
+        cout<<"derived class destructor called"<<endl;    
     }
 };
 
+int main(){
+    derived* d = new derived();
+    base *b = d; // upcasting
+    delete b;
+    // delete d; // no double free error
+    return 0;
+}
