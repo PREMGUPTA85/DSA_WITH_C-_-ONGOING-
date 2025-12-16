2dd10c75714c382720e68c58be12a238d1b895cd
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2025-12-16 23:55:52 +0530

    Add base class with constructor and destructor

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,10 @@
+#include<iostream>
+using namespace std;
 
+class base{
+    public: 
+    base(){
+        cout<<"base class constructor called"<<endl;    
     }
     ~base(){
         cout<<"base class destructor called"<<endl;    
