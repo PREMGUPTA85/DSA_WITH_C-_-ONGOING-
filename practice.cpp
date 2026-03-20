4fbae18741da8b653c0871f23f90d3c773c4fc5f
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-20 15:27:30 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,17 @@
 
+        swap(arr[i], arr[largest]);
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
         // Move current root to end
         swap(arr[0], arr[i]);
         // call max heapify on the reduced heap
