4f491c1ce9122f582a24cbade7de2baabfa44681
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-22 22:43:51 +0530

    Update tictac.cpp

--- tictac.cpp
+++ tictac.cpp
@@ -1,4 +1,24 @@
 
+		else
+		if (square[0] == square [3]  && square[3] == square[6] )
+			{	if ( square [0] == 'X' )			
+			return 1;
+			else
+			return 2; 
+		}
+		else
+		if (square[1] == square [4]  && square[4] == square[7] )
+			{	if ( square [1] == 'X' )			
+			return 1;
+			else
+			return 2; 
+		}
+	else
+		if (square[2] == square [5]  && square[5] == square[8] )
+			{	if ( square [2] == 'X' )			
+			return 1;
+			else
+			return 2; 
 		}
 else
 		if (square[0] == square [4]  && square[4] == square[8] )
