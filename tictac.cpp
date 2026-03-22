b7775b2b1e1185fdfb3eab79f34e075393f81acd
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-22 22:46:13 +0530

    Update tictac.cpp

--- tictac.cpp
+++ tictac.cpp
@@ -1,4 +1,7 @@
-(square[0] == square [1]  && square[1] == square[2] )
+e[9] = {'0','1','2','3','4','5','6','7','8'};
+int checkwin()
+{
+		if (square[0] == square [1]  && square[1] == square[2] )
 		{	if ( square [0] == 'X' )			
 			return 1;
 			else
