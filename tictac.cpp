19d24f9c3c6074bbb854ad1881d4570dc9397fbc
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-22 22:43:32 +0530

    Update tictac.cpp

--- tictac.cpp
+++ tictac.cpp
@@ -1,49 +1,4 @@
-#include <iostream>
 
-using namespace std;
-char square[9] = {'0','1','2','3','4','5','6','7','8'};
-int checkwin()
-{
-		if (square[0] == square [1]  && square[1] == square[2] )
-		{	if ( square [0] == 'X' )			
-			return 1;
-			else
-			return 2; 
-		}		
-		else
-		if (square[3] == square [4]  && square[4] == square[5] )
-			{	if ( square [3] == 'X' )			
-			return 1;
-			else
-			return 2; 
-		}
-		else
-		if (square[6] == square [7]  && square[7] == square[8] )
-			{	if ( square [6] == 'X' )			
-			return 1;
-			else
-			return 2; 
-		}
-		else
-		if (square[0] == square [3]  && square[3] == square[6] )
-			{	if ( square [0] == 'X' )			
-			return 1;
-			else
-			return 2; 
-		}
-		else
-		if (square[1] == square [4]  && square[4] == square[7] )
-			{	if ( square [1] == 'X' )			
-			return 1;
-			else
-			return 2; 
-		}
-	else
-		if (square[2] == square [5]  && square[5] == square[8] )
-			{	if ( square [2] == 'X' )			
-			return 1;
-			else
-			return 2; 
 		}
 else
 		if (square[0] == square [4]  && square[4] == square[8] )
