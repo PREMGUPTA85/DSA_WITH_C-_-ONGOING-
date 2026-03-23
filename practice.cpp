7f936d33fe01485e3b62b2f8d9c3232661a73755
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-03-23 21:37:01 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,4 +1,29 @@
 
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i) {
+        swap(arr[i], arr[largest]);
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    // Build heap (rearrange array)
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    // One by one extract an element from heap
+    for (int i = n - 1; i >= 0; i--) {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+        // call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
 
 int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
