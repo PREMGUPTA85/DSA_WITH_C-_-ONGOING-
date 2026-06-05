d9b7817bc904bb23817270af405d77cd4dbbc6d0
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-06-05 22:18:33 +0530

    Update practice.cpp

--- practice.cpp
+++ practice.cpp
@@ -1,5 +1,4 @@
 
-    public String longestCommonPrefix(String[] strs) {
         String prefix = strs[0];
         for(int i = 1; i < strs.length; i++) {
             while(strs[i].indexOf(prefix) != 0) {
