cbbb0b80071005461f90abb541d18778080f58bd
PREM KUMAR GUPTA <premgupta100705@gmail.com>
2026-09-07 22:05:20 +0530

    Add findMedianSortedArrays function to Solution class

--- practice.cpp
+++ practice.cpp
@@ -1,6 +1,7 @@
 class Solution {
 
 public:
+
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
         if(nums1.size()>nums2.size()) return findMedianSortedArrays(nums2,nums1);
 
